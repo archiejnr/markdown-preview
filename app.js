1376b9aa9bcd6950226fb2c53a6c32bc618e974a
Archbold <archiboldpatsanza@gmail.com>
2019-07-31 12:36:18 +0300

    cross origin att

---
+++ app.js
@@ -0,0 +1,9 @@
+class App extends React.Component {
+  render(){
+    return (<div>hello react</div>)
+  }
+}
+
+ReactDOM.render(<div>hello world</div>,document.querySelector('#root'))
+
+console.log('everything bho zvekuti')
